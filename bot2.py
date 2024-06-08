db1ec08db897bd287ecad85dfc637e0b42b5afb7
CNath01 <92951005+CNath01@users.noreply.github.com>
2024-06-08 15:49:10 -0500

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 
+print("lo que sea")
 # Configuración de intents para obtener el contenido de los mensajes
 intents = discord.Intents.default()
 intents.message_content = True
